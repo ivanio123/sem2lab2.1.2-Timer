4275117247f25a0fdbe0213da8a12e63ab11767f
Ivan Dzherikhov <jodsonjod@gmail.com>
2021-03-30 19:57:39 +0300

    Добавьте файлы проекта.

---
+++ sem2.7.1.2(Timer)/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace sem2._7._1._2_Timer_
+{
+    class Program
+    {
+        delegate void Delegate(int sec);
+
+        public delegate void Methods();
+        static void Main(string[] args)
+        {
+            Console.Write("Введiть кiлькiсть секунд: ");
+            int sec = int.Parse(Console.ReadLine());
+            Timer timer = new Timer();
+            Delegate del = timer.Start;
+            del.Invoke(sec);
+        }
+    }
+}
---
+++ sem2.7.1.2(Timer)/Timer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using static sem2._7._1._2_Timer_.Program;
+
+namespace sem2._7._1._2_Timer_
+{
+    class Timer
+    {
+        public void Start(int sec)
+        {
+            Methods[] methods = new Methods[] { MethodOne, MethodTwo, MethodThree };
+            Random rand = new Random();
+            while (true)
+            {
+                Thread.Sleep(sec * 1000);
+                int i = rand.Next(0, 3);
+                methods[i]();
+            }
+        }
+        private static void MethodOne()
+        {
+            Console.WriteLine("Метод 1.");
+        }
+        private static void MethodTwo()
+        {
+            Console.WriteLine("Метод 2.");
+        }
+        private static void MethodThree()
+        {
+            Console.WriteLine("Метод 3.");
+        }
+    }
+}
